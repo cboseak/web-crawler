00a655a3a41c51f3975655c83a522b3bf690b456
Morgan Brenner <brennemo@oregonstate.edu>
2017-05-20 12:38:20 -0400

    Gradient masks + fixes
    Increased font size
    Only check last element for keyword
    Simplified on-clicks - only apply to top rectangle with gradient

--- scrape.js
+++ scrape.js
@@ -84,8 +84,19 @@ app.directive('graphDirective', function($parse) {
       data: '=',
       keyword: '='
     }, 
-    link: function(scope, element, attrs) { 
-      var colors = ['rgb(255, 95, 96)', 'rgb(255, 127, 127)', 'rgb(188,160,151)', 'rgb(120,193, 174)', 'rgb(112,165,192)', 'rgb(104, 136, 210)', 'rgb(136,126,199)', 'rgb(167, 115, 188)', 'rgb(219, 119, 192)', 'rgb(237,107,144)'];
+link: function(scope, element, attrs) { 
+      var colors = [
+          {background: 'rgb(255, 95, 96)', gradient: 'rgb(237,107,144)'}, 
+          {background: 'rgb(255, 127, 127)', gradient: 'rgb(255, 95, 96)'}, 
+          {background: 'rgb(188,160,151)', gradient: 'rgb(255, 127, 127)'}, 
+          {background: 'rgb(120,193, 174)', gradient: 'rgb(188,160,151)'}, 
+          {background: 'rgb(112,165,192)', gradient: 'rgb(120,193, 174)'}, 
+          {background: 'rgb(104, 136, 210)', gradient: 'rgb(112,165,192)'}, 
+          {background: 'rgb(136,126,199)', gradient: 'rgb(104, 136, 210)'}, 
+          {background: 'rgb(167, 115, 188)',  gradient: 'rgb(136,126,199)'}, 
+          {background: 'rgb(219, 119, 192)',  gradient: 'rgb(167, 115, 188)'}, 
+          {background: 'rgb(237,107,144)', gradient: 'rgb(255, 95, 96)'}
+          ];
       var keyColor = 'rgb(226, 226, 52)';
       var numColors = colors.length;
     
@@ -111,16 +122,35 @@ app.directive('graphDirective', function($parse) {
       //defs for gradients/masks 
       var defs = d3.select('.chart').append('defs');
 
-      var linearGradient = defs.append('linearGradient').attr('id', 'test-gradient').attr('x2', '0%').attr('y2', '100%');
+      var linearGradient = defs.append('linearGradient').attr('id', 'cell-gradient').attr('x2', '0%').attr('y2', '100%');
       var stop1 = linearGradient.append('stop').attr('offset', '0').attr('stop-color', 'rgb(255, 255, 255)').attr('stop-opacity', '0');
-      var stop2 = linearGradient.append('stop').attr('offset', '1').attr('stop-color', 'rgb(255, 255, 255)').attr('stop-opacity', '1');
+      var stop2 = linearGradient.append('stop').attr('offset', '.25').attr('stop-color', 'rgb(255, 255, 255)').attr('stop-opacity', '0');
+      var stop3 = linearGradient.append('stop').attr('offset', '1').attr('stop-color', 'rgb(255, 255, 255)').attr('stop-opacity', '1');
 
-      var mask = defs.append('mask').attr('id', 'test-mask');
+      var mask = defs.append('mask').data(data).attr('id', 'cell-mask');
+
+      var maskRow = mask.selectAll('.mask-row')
+        .data(data)
+        .enter().append('g')
+        .attr('class', 'mask-row')
+        .attr('id', function(d, i) {return 'mask-g' + i;})
+        .attr('transform', function(d, i) { return 'translate(0, ' + i * barHeight + ')'; });  
+
+      var maskCell = maskRow.selectAll('.mask-cell')
+        .data(function(d) { return d; })
+        .enter().append('rect')
+        .attr('class','mask-cell')
+        .attr('id', function(d, i) {return 'mask-cell' + i + '-' + d.row;})
+        .attr('x', function(d, i) { return i * barWidth; })
+        .attr('y', 0)
+        .attr('width', barWidth )
+        .attr('height', barHeight - 20) 
+        .style('fill', 'url(#cell-gradient)');
       
       var grid = d3.select('.chart')
         .attr('width', width + 400)   //buffer of 400px on right
         .attr('height', rows * barHeight > 800 ? rows * barHeight + 400 : 800) //at least 800px; buffer of 400px at bottom 
- 
+
       var row = grid.selectAll('.row')
         .data(data)
         .enter().append('g')
@@ -128,31 +158,18 @@ app.directive('graphDirective', function($parse) {
         .attr('id', function(d, i) {return 'g' + i;})
         .attr('transform', function(d, i) { return 'translate(0, ' + i * barHeight + ')'; });
 
-      var column = row.selectAll('.column')
+      var cell1 = row.selectAll('.bottom-cell')
         .data(function(d) { return d; })
         .enter().append('rect')
-        .attr('class','square')
-        .attr('id', function(d, i) {return 'square' + i + '-' + d.row;})
+        .attr('class','bottom-cell')
+        .attr('id', function(d, i) {return 'bottom-cell' + i + '-' + d.row;})
         .attr('x', function(d, i) { return i * barWidth; })
         .attr('y', 0)
         .attr('width', barWidth )
         .attr('height', barHeight - 20) //effectively bottom margin of 10px
         .style('fill', function(d, i) {
           return getCellColor(d, i);    //serpentine-ish color pattern 
-        })
-        .on('click', function(d, i) {
-          var coords = d3.mouse(this);          //get coordinates of cursor 
-          //d3.selectAll('.child').remove();      //temporary fix for closing upon click outside window
-          handleOnClick(d, i, coords); 
-        })
-        .on('mouseover',function(d, i) {
-          d3.select(this).style('cursor', 'pointer'); 
-          d3.select(this).transition().duration(500).style('fill', d3.rgb(getCellColor(d, i)).darker(1));
-        })
-        .on('mouseout',function(d, i) {     
-          d3.select(this).transition().duration(500).style('fill', getCellColor(d, i));
         });
-        
 
       var text = row.selectAll('.label')
         .data(function(d) { return d; })
@@ -164,19 +181,34 @@ app.directive('graphDirective', function($parse) {
           .attr('text-anchor', 'start')
           .text(function(d) { return d.url.root; })
           .style('fill', function(d) {if (d.hasKeyword) {return 'rgb(105,105,105)';}})
-          .on('click', function(d, i) {
-              var coords = d3.mouse(this);  //get coordinates of cursor 
-              handleOnClick(d, i, coords); 
-            })
-          .on('mouseover',function(d, i) {
-            d3.select(this).style('cursor', 'pointer');
-            d3.select('#square' + i + '-' + d.row).transition().duration(500).style('fill', d3.rgb(getCellColor(d, i)).darker(1));
-          })
-          .on('mouseout',function(d, i) {     
-            d3.select('#square' + i + '-' + d.row).transition().duration(500).style('fill', getCellColor(d, i));
-         })
           .call(wrap, barWidth - 20);
       
+      var cell2 = row.selectAll('.top-cell')
+        .data(function(d) { return d; })
+        .enter().append('rect')
+        .attr('class','top-cell')
+        .attr('id', function(d, i) {return 'top-cell' + i + '-' + d.row;})
+        .attr('x', function(d, i) { return i * barWidth; })
+        .attr('y', 0)
+        .attr('width', barWidth )
+        .attr('height', barHeight - 20) //effectively bottom margin of 10px
+        .style('fill', function(d, i) {
+          return getGradientColor(d, i);    //serpentine-ish color pattern 
+        })
+        .attr('mask', 'url(#cell-mask)')
+        .attr('opacity', '1')
+        .on('mouseover',function() {
+            d3.select(this).style('cursor', 'pointer'); 
+            d3.select(this).transition().duration(500).attr('opacity', '0');
+          })
+        .on('mouseout',function() {
+          d3.select(this).transition().duration(500).attr('opacity', '1');
+        })
+        .on('click', function(d, i) {
+          var coords = d3.mouse(this);          //get coordinates of cursor 
+          if (activeChild == '') { handleOnClick(d, i, coords) }; 
+        });
+
         //build a matrix of objects with URLs and rows for y axis positioning 
         function listToMatrix(list, cols) {
             var matrix = [], i, j;
@@ -193,46 +225,36 @@ app.directive('graphDirective', function($parse) {
                   keyIdx: -1                  //if hasKeyword == true and keyword is in root, keyIdx remains -1
                 });
             }
-          
-            for (i = 0; i < matrix.length; i++) {
-              if (i % 2 == 1)                //reverse every other row to create serpentine pattern 
-                matrix[i].reverse();
-            }   
+
             if (keyword) {
               console.log(findKeyword(matrix));  
             }  
             return matrix;
         }
 
-        //find keyword location if applicable, work backwards since it's most likely at the end 
+        //find keyword location in URL, last if applicable
         function findKeyword(matrix) {
-            for (var i = matrix.length - 1; i >= 0; i--) {
-              for (var j = matrix[i].length - 1; j >= 0; j--) {
-               //console.log(i + ', ' + j);
-                if (matrix[i][j].url.root.includes(keyword)) {
-                  //console.log('found in root: ' + matrix[i][j].url.root);
-                  matrix[i][j].hasKeyword = true; 
-                  return true;
-                }
+          var i = matrix.length-1;
+          var j = matrix[i].length - 1;
+          if (matrix[i][j].url.root.includes(keyword)) {
+            matrix[i][j].hasKeyword = true; 
+            return true;
+          }
 
-                for (var k = 0; k < matrix[i][j].url.links.length; k++) {
-                  //console.log(k);
-                  if (matrix[i][j].url.links[k].includes(keyword)) {
-                   // console.log('found in link: ' + matrix[i][j].url.links[k]);
-                    matrix[i][j].hasKeyword = true; 
-                    matrix[i][j].keyIdx = k;
-                    return true;
-                  }
-                }               
-              }                   
-            }                                      
+          for (var k = matrix[i][j].url.links.length - 1; k >= 0 ; k--) {
+            if (matrix[i][j].url.links[k].includes(keyword)) {
+              matrix[i][j].hasKeyword = true; 
+              matrix[i][j].keyIdx = k;
+              return true;
+            }
+          }                                                                    
         }
       
         //text wrapping adapted from https://bl.ocks.org/mbostock/7555321
         function wrap(text, width) {
           text.each(function() {
             var text = d3.select(this),
-              words = text.text().match(/.{1,18}/g).reverse(),  //split into 18-character chunks 
+              words = text.text().match(/.{1,12}/g).reverse(),  //split into 18-character chunks 
               word,
               line = [],
               lineNumber = 0,
@@ -241,43 +263,49 @@ app.directive('graphDirective', function($parse) {
               y = 4,
               dy = parseFloat(text.attr('dy')),
               tspan = text.text(null).append('tspan').attr('id', 'line'+lineNumber+'-'+text.attr('id')).attr('x', x).attr('y', y).attr('dy', dy + 'em');
-            while (word = words.pop()) {  
-              line.push(word);
-              tspan.text(line.join(' '));
-              if (tspan.node().getComputedTextLength() > width) {
-                line.pop();
+            while (word = words.pop()) { 
+              if (lineNumber < 2) {
+                line.push(word);
                 tspan.text(line.join(' '));
-                line = [word];  
-                tspan = text.append('tspan').attr('id', 'line'+lineNumber).attr('x', x).attr('y', y).attr('dy', ++lineNumber * lineHeight + dy + 'em').text(word);
-              }
+                if (tspan.node().getComputedTextLength() > width) {
+                  line.pop();
+                  tspan.text(line.join(' '));
+                  line = [word];  
+                  tspan = text.append('tspan').attr('id', 'line'+lineNumber).attr('x', x).attr('y', y).attr('dy', ++lineNumber * lineHeight + dy + 'em').text(word);
+                }
+              } else { break; } 
             }
           });
         }
 
         //highlights link/child containing keyword, assigns color based on pattern otherwise 
         function getCellColor(d, i) {  
-          if (d.hasKeyword == true) {
-            return keyColor;
-          }
+          if (d.hasKeyword == true) { return keyColor; }
           var colorIdx = d.row % 2 == 0 ? (Math.abs(numColors -1 - (i % (numColors)) + d.row) % numColors) : ((i % (numColors) + d.row) % numColors);
-          return colors[colorIdx]; 
+          return colors[colorIdx].background; 
+        }
+
+        //highlights link/child containing keyword, assigns color based on pattern otherwise 
+        function getGradientColor(d, i) {  
+          if (d.hasKeyword == true) { return 'rgb(239, 144, 62)'; }
+          var colorIdx = d.row % 2 == 0 ? (Math.abs(numColors -1 - (i % (numColors)) + d.row) % numColors) : ((i % (numColors) + d.row) % numColors);
+          return colors[colorIdx].gradient; 
         }
       
         function handleOnClick(d, i, coords) {
-          //unhide or increase opacity from 0 to 1 if visited == true
+          //unhide if visited already
           if (d.visited == true) {
-            //d3.select('#g-child0-0').attr('opacity', '1');
              $('#g-child' + i + '-' + d.row).show();
              activeChild = '#g-child' + i + '-' + d.row;
-             console.log(activeChild);
-             //$('#g-child' + i + '-' + d.row).toggleClass('active-child');
           }
 
+          //construct child list if unvisited 
           if (d.visited == false) {
             var childrenDisplay = grid.append('g')
               .attr('class', 'child')
               .attr('id', 'g-child' + i + '-' + d.row)
 
+              //rectangles behind links 
               var line = 0;
               for (var j = 0; j < d.url.links.length; j++) {
                  childrenDisplay.append('rect')
@@ -285,22 +313,24 @@ app.directive('graphDirective', function($parse) {
                 .attr('id', 'rect-child' + i + '-' + d.row)
                 .attr('x', coords[0])
                 .attr('y', coords[1] + line++ * 16)
-                .attr('width', barWidth)
+                .attr('width', barWidth * 2)
                 .attr('height', 16) 
                 .attr('stroke', '#fff')
-                .style('fill', d3.rgb(d3.select('#square' + i + '-' + d.row).style('fill')).brighter(1));
+                .style('fill', d3.rgb(d3.select('#bottom-cell' + i + '-' + d.row).style('fill')).brighter(1))
                         
                 /*
                 .on('mouseover',function() {
                   d3.select(this).style('cursor', 'pointer');
-                  d3.select(this).transition().duration(100).style('fill', d3.rgb(d3.select('#square' + i + '-' + d.row).style('fill')).darker(1))
+                  d3.select(this).transition().duration(100).style('fill', d3.rgb(d3.select('#bottom-cell' + i + '-' + d.row).style('fill')).darker(1))
                 })
                 .on('mouseout',function() {     
-                  d3.select(this).transition().duration(100).style('fill', d3.rgb(d3.select('#square' + i + '-' + d.row).style('fill')).brighter(1))
+                  d3.select(this).transition().duration(100).style('fill', d3.rgb(d3.select('#bottom-cell' + i + '-' + d.row).style('fill')).brighter(1))
                 });
                 */
+              
               }
 
+              //text w/ links 
               line = 0; 
               for (var j = 0; j < d.url.links.length; j++) {
                 childrenDisplay.append('a').attr('class', 'child').attr('href', 'http://' + d.url.links[j] ).attr('target', '_blank')
@@ -322,23 +352,22 @@ app.directive('graphDirective', function($parse) {
                       if (d.hasKeyword == true || d3.hsl(d3.select('#rect-child' + i + '-' + d.row).style('fill')).l > 0.8) {
                         return 'rgb(105,105,105)';    //gray text for lighter backgrounds 
                       }
-                    });
+                    })
+                    
                     /*
                     .on('mouseover',function() {
                       d3.select(this).style('cursor', 'pointer');
-                      d3.select('#rect-child' + i + '-' + line).transition().duration(100).style('fill', d3.rgb(d3.select('#square' + i + '-' + d.row).style('fill')).darker(1))
+                      d3.select('#rect-child' + i + '-' + line).transition().duration(100).style('fill', d3.rgb(d3.select('#bottom-cell' + i + '-' + d.row).style('fill')).darker(1))
                     })
                     .on('mouseout',function() {     
-                      d3.select('#rect-child' + i + '-' + line).transition().duration(100).style('fill', d3.rgb(d3.select('#square'+ i + '-' + d.row).style('fill')).brighter(1))
+                      d3.select('#rect-child' + i + '-' + line).transition().duration(100).style('fill', d3.rgb(d3.select('#bottom-cell'+ i + '-' + d.row).style('fill')).brighter(1))
                     });
                     */
+                    
               }
               d.visited = true;
               activeChild = '#g-child' + i + '-' + d.row;
-              console.log(activeChild);
-            }
-          
-          //console.log(d.visited);  
+            } 
         }
 
         //hide active child url list on click outside of list and clear activeChild 
